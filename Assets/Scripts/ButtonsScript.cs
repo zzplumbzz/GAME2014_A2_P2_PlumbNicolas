5bc56b7049b13bfabd75398cf8f85bc20dce232e
Nicolas Plumb <nicolasplumb@yahoo.com>
2020-11-20 11:52:24 -0500

    commit

---
+++ Assets/Scripts/ButtonsScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonsScript : MonoBehaviour
+{
+    public void toGameScene()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    public void toInstructionScene()
+    {
+        SceneManager.LoadScene("Instructions");
+    }
+
+    public void toMenuScene()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void toGameOverScene()
+    {
+        SceneManager.LoadScene("GameOver");
+    }
+}
